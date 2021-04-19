706253e8170cb9c5a29c4515cc52809f079a404b
Bruno <brunorodriguez540@gmail.com>
2021-04-19 17:27:34 -0300

    Rest API  funcional

--- LabJavaEE/LabJAVAEE/src/main/java/com/beans/UsuarioBean.java
+++ LabJavaEE/LabJAVAEE/src/main/java/com/beans/UsuarioBean.java
@@ -1,5 +1,7 @@
 package com.beans;
 
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -9,17 +11,21 @@ import java.util.Map;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.primefaces.shaded.commons.io.IOUtils;
 
 import com.dao.UsuarioDAO;
 import com.google.gson.Gson;
 import com.model.Usuario;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.api.json.JSONConfiguration;
+
+
 
 @ManagedBean(name = "UsuarioBean")
 @RequestScoped
@@ -44,40 +50,64 @@ public class UsuarioBean {
 	}
 	
 	public String guardar (Usuario Usuario) {
-		//guarda la fecha de registro
-		UsuarioDAO UsuarioDAO= new UsuarioDAO();
-		UsuarioDAO.guardar(Usuario);
-		return  "/faces/index.xhtml";
+		
+		String urlRestService = "http://localhost:8080/rest-lab/api/ejemplo/registrarse";
+		Client client = ClientBuilder.newClient();
+        Form form = new Form();
+        form.param("nick", Usuario.getNick());
+        form.param("nombre", Usuario.getNombres());
+        form.param("apellido", Usuario.getApellidos());
+        form.param("direccion", Usuario.getDireccion());
+        form.param("email", Usuario.getEmail());
+        form.param("telefono", Usuario.getTelefono());
+        WebTarget target= client.target(urlRestService);
+        Response response = target.request().post(Entity.entity(form, MediaType.APPLICATION_FORM_URLENCODED));
+        String response2 = response.readEntity(String.class);
+        System.out.println("La respuesta es: " + response2);
+        
+        return  "/faces/index.xhtml";
 	}
 
 	public List<Usuario> obtenerUsuarios() {
+	
 		String urlRestService = "http://localhost:8080/rest-lab/api/ejemplo/usuarios";
-		ClientConfig clientConfig = new DefaultClientConfig();
-        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
-        Client client = Client.create(clientConfig);
-        WebResource webResource = client.resource(urlRestService);  
-        String response2 = webResource.get(String.class);
+		Client client = ClientBuilder.newClient();
+		WebTarget target= client.target(urlRestService);
+        Response response = target.request().get();
+        String response2 = response.readEntity(String.class);
         Usuario[] u = new Gson().fromJson(response2, Usuario[].class);
         List<Usuario> datos = Arrays.asList(u);
         return datos;
 	}
 
-	public String editar(String nick) {
-		UsuarioDAO UsuarioDAO = new UsuarioDAO();
-		Usuario c = new Usuario();
-		c = UsuarioDAO.buscar(nick);
-		System.out.println("******************************************");
-		System.out.println(c);
-
+	public String editar(String Usuario) {
+		String urlRestService = "http://localhost:8080/rest-lab/api/ejemplo/usuario/" + Usuario;
+		Client client = ClientBuilder.newClient();
+		WebTarget target= client.target(urlRestService);
+		Response response = target.request().get();
+        String response2 = response.readEntity(String.class);
+        Usuario u = new Gson().fromJson(response2, Usuario.class);
 		Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
-		sessionMap.put("Usuario", c);
+		sessionMap.put("Usuario", u);
 		return "/faces/editar.xhtml";
 	}
 
 	public String actualizar(Usuario Usuario) {
 		//guarda la fecha de actualizacion
-		UsuarioDAO UsuarioDAO = new UsuarioDAO();
-		UsuarioDAO.editar(Usuario);
+		//UsuarioDAO UsuarioDAO = new UsuarioDAO();
+		//UsuarioDAO.editar(Usuario);
+		String urlRestService = "http://localhost:8080/rest-lab/api/ejemplo/editar";
+		Client client = ClientBuilder.newClient();
+		WebTarget target= client.target(urlRestService);
+		Form form = new Form();
+        form.param("nick", Usuario.getNick());
+        form.param("nombre", Usuario.getNombres());
+        form.param("apellido", Usuario.getApellidos());
+        form.param("direccion", Usuario.getDireccion());
+        form.param("email", Usuario.getEmail());
+        form.param("telefono", Usuario.getTelefono());
+        Response response = target.request().post(Entity.entity(form, MediaType.APPLICATION_FORM_URLENCODED));
+        String response2 = response.readEntity(String.class);
 		return "/faces/index.xhtml";
 	}
 
--- rest-lab/src/main/java/com/lab/java/RecursosRest.java
+++ rest-lab/src/main/java/com/lab/java/RecursosRest.java
@@ -4,8 +4,10 @@ import javax.ws.rs.POST;
 
 import java.util.List;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -36,7 +38,59 @@ public class RecursosRest {
 			return Response.status(Response.Status.NOT_FOUND).build();
 		}
 	}
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/usuario/{nick}")
+	public Response obtenerusuario(@PathParam("nick") String nick) {
+		UsuarioDAO UsuarioDAO = new UsuarioDAO();
+		Usuario user = UsuarioDAO.buscar(nick);
+		if(user!=null) {
+			return Response.ok(user).build();
+		}
+		else {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+	}
 	
-	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/registrarse")
+	public Response registro(@FormParam("nick") String nick, @FormParam("nombre") String nombre, @FormParam("apellido") String apellido, @FormParam("direccion") String direccion, @FormParam("email") String email, @FormParam("telefono") String telefono) {
+		if(nick!=null && !nick.equals("") && nombre!=null && !nombre.equals("")&& apellido!=null && !apellido.equals("")&& direccion!=null && !direccion.equals("")&& telefono!=null && !telefono.equals("")&& email!=null && !email.equals("")) {
+			UsuarioDAO u = new UsuarioDAO();
+			Usuario u1 = new Usuario();
+			u1.setNick(nick);
+			u1.setNombres(nombre);
+			u1.setApellidos(apellido);
+			u1.setDireccion(direccion);
+			u1.setTelefono(telefono);
+			u1.setEmail(email);
+			u.guardar(u1);
+			return Response.ok("SE CREO CORRECTAMENTE EL USUARIO").build();
+		}
+		else {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+	}
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/editar")
+	public Response editar(@FormParam("nick") String nick, @FormParam("nombre") String nombre, @FormParam("apellido") String apellido, @FormParam("direccion") String direccion, @FormParam("email") String email, @FormParam("telefono") String telefono) {
+		if(nombre!=null && !nombre.equals("")&& apellido!=null && !apellido.equals("")&& direccion!=null && !direccion.equals("")&& telefono!=null && !telefono.equals("")&& email!=null && !email.equals("")) {
+			UsuarioDAO u = new UsuarioDAO();
+			Usuario u1 = new Usuario();
+			u1.setNick(nick);
+			u1.setNombres(nombre);
+			u1.setApellidos(apellido);
+			u1.setDireccion(direccion);
+			u1.setTelefono(telefono);
+			u1.setEmail(email);
+			u.editar(u1);
+			return Response.ok("SE EDITO CORRECTAMENTE EL USUARIO").build();
+		}
+		else {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+	}
 }
 
