61540d6958a7ef60925a8f6b26370236ac9bcacc
pablogb83 <66219692+pablogb83@users.noreply.github.com>
2021-04-23 14:31:39 -0300

    ultimo

--- demo_lab/src/main/java/com/tallerjava/controller/JuegoBean.java
+++ demo_lab/src/main/java/com/tallerjava/controller/JuegoBean.java
@@ -1,14 +1,23 @@
 package com.tallerjava.controller;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.primefaces.model.file.UploadedFile;
 
+import com.google.gson.Gson;
 import com.tallerjava.dao.JuegoDAO;
 import com.tallerjava.model.Juego;
 
@@ -64,20 +73,50 @@ public class JuegoBean {
             juego.setRutaImg(file.getFileName());
         }
 		
-		JuegoDAO juegoDAO= new JuegoDAO();
-		juegoDAO.copiarImagenServidor(file);
-		juegoDAO.guardar(juego);
+		String urlRestService = "http://localhost:8080/rest-lab/api/ejemplo/registrarJuego";
+		Client client = ClientBuilder.newClient();
+        Form form = new Form();
+        form.param("nombre", juego.getNombre());
+        form.param("descripcion", juego.getDescripcion());
+        form.param("rutaImg", juego.getRutaImg());
+        form.param("precio", String.valueOf(juego.getPrecio()));
+        WebTarget target= client.target(urlRestService);
+        Response response = target.request().post(Entity.entity(form, MediaType.APPLICATION_FORM_URLENCODED));
+        String response2 = response.readEntity(String.class);
 		return  "/faces/index.xhtml";
 	}
-	
+	/*	
 	public List<Juego> obtenerJuegos(){
 		JuegoDAO juegoDAO= new JuegoDAO();
 		return juegoDAO.obtenerJuegos();
-	}
+	}*/
 	
+	public List<Juego> obtenerJuegos(){
+		String urlRestService = "http://localhost:8080/rest-lab/api/ejemplo/obtenerJuegos";
+		Client client = ClientBuilder.newClient();
+		WebTarget target= client.target(urlRestService);
+        Response response = target.request().get();
+        String response2 = response.readEntity(String.class);
+        Juego[] j = new Gson().fromJson(response2, Juego[].class);
+        List<Juego> datos = Arrays.asList(j);
+        return datos;
+	}
+	/*
 	public List<Juego> buscadorJuego(String busqueda){
 		JuegoDAO juegoDAO= new JuegoDAO();
 		return juegoDAO.buscarJuegos(busqueda);
+	}*/
+	
+	public List<Juego> buscadorJuego(String busqueda){
+		System.out.println("Estoy en buscadorJuego() " + busqueda);	
+		String urlRestService = "http://localhost:8080/rest-lab/api/ejemplo/buscadorJuegos/" + busqueda;
+		Client client = ClientBuilder.newClient();
+		WebTarget target= client.target(urlRestService);
+        Response response = target.request().get();
+        String response2 = response.readEntity(String.class);
+        Juego[] j = new Gson().fromJson(response2, Juego[].class);
+		List<Juego> datos = Arrays.asList(j);
+		return datos;
 	}
 	
 	public String listarJuegos() {
@@ -87,17 +126,32 @@ public class JuegoBean {
 	}
 	
 	public String resultadoBusqueda() {
+		System.out.println("Estoy en resultadoBusqueda() " + busqueda);
 		this.juegos=this.buscadorJuego(busqueda);
 		return "/faces/paginasJuegos/listarJuego.xhtml";
 	}
 	
+	/*
 	public String buscarJuego(int id) {
 		JuegoDAO juegoDAO= new JuegoDAO();
 		Juego j = juegoDAO.buscar(id);
 		Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
 		sessionMap.put("juego",j);
 		return "/faces/paginasJuegos/comprarJuego.xhtml";
+	}*/
+	
+	public String buscarJuego(int id) {
+		String urlRestService = "http://localhost:8080/rest-lab/api/ejemplo/buscarJuego";
+		Client client = ClientBuilder.newClient();
+        Form form = new Form();
+        form.param("id", String.valueOf(id));
+        WebTarget target= client.target(urlRestService);
+        Response response = target.request().post(Entity.entity(form, MediaType.APPLICATION_FORM_URLENCODED));
+        String response2 = response.readEntity(String.class);
+        Juego j = new Gson().fromJson(response2, Juego.class);
+		Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+		sessionMap.put("juego",j);
+		return "/faces/paginasJuegos/comprarJuego.xhtml";
 	}
 	
-
 }
